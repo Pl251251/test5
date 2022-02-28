aa47ac9e0ddfb52cca7cd81d2070239590470b2d
Pl251251 <91552726+Pl251251@users.noreply.github.com>
2022-02-28 15:18:06 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,54 @@
 import cv2
 import numpy as np
+from PIL import Image
+import math
 
+#pillow
+im = Image.open('test_l3.jpeg')
 
-img = cv2.imread('test_l3.jpeg', 1)
-width = img.shape[1]
-height = img.shape[0]
-print(width)
-print(height)
+w,h =im.size
+im2 = Image.new(im.mode, (22*w/12, 15*h/8), (255,255,255))
+im2.paste(im, (int(5*w/12), int(3.5*h/8)))
+
+im2.save("test.jpg", quality = 100)
+#opencv
+
+img = cv2.imread('test.jpg', 1)
+
 # Locate points of the documents
 # or object which you want to transform
-pts1 = np.float32([[0, 260], [640, 260], [0, 400], [640, 400]])
-pts2 = np.float32([[0, 0], [400, 0], [0, 640], [400, 640]])
+pts1 = np.float32([[int(w/12), int(h/8)], [int(640), int(260)], [int(0), int(400)], [int(640), int(400)]])
+pts2 = np.float32([[0, 0], [400, 0], [0, 400], [400, 400]])
 
 # Apply Perspective Transform Algorithm
 matrix = cv2.getPerspectiveTransform(pts1, pts2)
-result = cv2.warpPerspective(img, matrix, (500, 600))
+result = cv2.warpPerspective(img, matrix, (400, 400))
 # Wrap the transformed image
 cv2.imshow('frame', img)  # Initial Capture
 cv2.imshow('frame1', result)  # Transformed Capture
 
+#finding a circle
+blur= cv2.medianBlur(resized_,7)
+gray = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)
+try:
+    circles = cv2.HoughCircles(gray,cv2.HOUGH_GRADIENT,1,20,param1=50,param2=30,minRadius=246,maxRadius=265)
+    info = np.uint16(np.around(circles))
+    a=400
+
+    for i in info[0,:]:
+        if i[2] < a:
+            a = i[2]
+            b= i
+    cv2.circle(resized_,(b[0],b[1]),b[2],(0,0,255),2)
+    cv2.circle(resized_,(b[0],b[1]),2,(0,0,255),3)
+
+except:
+    pass
+
+
+cv2.imshow('oval', resized_)
+cv2.waitKey(5000)
+cv2.destroyAllWindows()
 
 cv2.imwrite("test.jpg", result, [cv2.IMWRITE_PNG_COMPRESSION, 0])
 cv2.waitKey(0)
