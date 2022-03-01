27b9b95f0ed79b5ea27dc3a4e47f510fda5bf402
Pl251251 <91552726+Pl251251@users.noreply.github.com>
2022-03-01 11:44:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import cv2
 import numpy as np
 from PIL import Image
 #import math
-
+"""
 #pillow
-im = Image.open('test_l3.jpg')
+im = Image.open('test_l3.jpeg')
 
 w,h =im.size
 print(w)
@@ -16,13 +16,13 @@ print(im2.size)
 
 im2.save("test.jpg", quality = 100)
 #opencv
-
-img = cv2.imread('test.jpg', 1)
+"""
+img = cv2.imread('test_l3.jpeg', 1)
 
 
 # Locate points of the documents
 # or object which you want to transform
-pts1 = np.float32([[0, int(5.4*h/8)], [int(14.25*w/12), 0], [int(7.75*w/12), int(15*h/8)], [int(22*w/12), int(6.7*h/8)]])
+pts1 = np.float32([[491, 703], [651, 245], [1985, 634], [2024, 122]])
 pts2 = np.float32([[0, 0], [800, 0], [0, 800], [800, 800]])
 
 # Apply Perspective Transform Algorithm
@@ -37,7 +37,7 @@ cv2.imwrite("test2.jpg", result)
 blur= cv2.medianBlur(result,7)
 gray = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)
 try:
-    circles = cv2.HoughCircles(gray,cv2.HOUGH_GRADIENT,1,20,param1=50,param2=30,minRadius=110,maxRadius=130)
+    circles = cv2.HoughCircles(gray,cv2.HOUGH_GRADIENT,1,20,param1=50,param2=30,minRadius=90,maxRadius=120)
     info = np.uint16(np.around(circles))
     a=400
     """
@@ -61,12 +61,12 @@ cv2.destroyAllWindows()
 
 # Apply Perspective Transform Algorithm
 matrix2 = cv2.getPerspectiveTransform(pts2, pts1)
-result2 = cv2.warpPerspective(result, matrix2, (int(22*w/12), int(16*h/8)))
+result2 = cv2.warpPerspective(result, matrix2, (2000, 1000))
 # Wrap the transformed image
 
 cv2.imshow('frame1', result2)  # Transformed Capture
 
-
+"""
 
 dim1 = result2.shape[1]
 dim2 = result2.shape[0]
@@ -76,10 +76,10 @@ dim5 = int((dim1/2) + w/2)-1
 dim6 = int((dim2/2) +h/2)-35
 img = result2[dim4:dim6,dim3:dim5]
 cv2.imwrite("final.jpg", img, [cv2.IMWRITE_PNG_COMPRESSION, 0])
-
+"""
 
 cv2.waitKey(5000)
-cv2.imshow('oval', img)
+cv2.imshow('oval', result2)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
